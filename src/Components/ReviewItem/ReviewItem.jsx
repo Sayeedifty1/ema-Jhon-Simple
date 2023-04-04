5b776b4824a43ce76793957819bdbd42f8fd6019
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-04-05 03:54:07 +0600

    font icon button added with css

--- src/Components/ReviewItem/ReviewItem.jsx
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './ReviewItem.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 const ReviewItem = ({product}) => {
-    const{name, quantity, id ,img} = product;
+    const{name, price, quantity, id ,img} = product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-name'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+                
+
+            </div>
+            <button className='btn-delete'><FontAwesomeIcon icon={faTrashAlt}/></button>
         </div>
     );
 };
