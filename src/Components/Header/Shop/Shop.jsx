8ac8d04d6b9625ea4c85308bb009a31915a1b798
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-16 02:24:31 +0600

    dynamic page number added

--- src/Components/Header/Shop/Shop.jsx
+++ src/Components/Header/Shop/Shop.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import { addToDb, deleteShoppingCart, getShoppingCart } from '../../../utilities/fakedb'
 import Cart from '../../Cart/Cart'
 import Product from '../../Product/Product'
 import './Shop.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 
 const Shop = () => {
 
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
   const [cart, setCart] = useState([]);
+  const [itemsPerPage , setItemsPerPage] = useState(10);
 
-  const handleClearCart = ()=>{
+  const options = [5 , 10, 20]
+  // adding pagination
+  const { totalProducts } = useLoaderData()
+  // const itemsPerPage = 10; make it dynamic
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
+
+  // const pageNumber = [];
+  // for (let i = 1; i <= totalPages; i++) {
+  //   pageNumber.push(i);
+  // }
+  const pageNumber = [...Array(totalPages).keys()];
+
+
+
+
+  const handleClearCart = () => {
     setCart([]);
     deleteShoppingCart();
-}
+  }
 
 
   useEffect(() => {
@@ -43,7 +60,7 @@ const Shop = () => {
 
         savedCart.push(addedProduct);
       }
-      
+
 
     }
     // step 5: set cart
@@ -55,14 +72,14 @@ const Shop = () => {
     // if product doesn't exist in the cart, set quantity = 1, 
     // if exist  update by 1
     const exists = cart.find(pd => pd._id == product._id);
-    if(!exists){
-      product.quantity=1;
-      newCart = [...cart ,product ]
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product]
     }
-    else{
-      exists.quantity = exists.quantity +1;
+    else {
+      exists.quantity = exists.quantity + 1;
       const remaining = cart.filter(pd => pd._id !== product._id);
-      newCart = [...remaining , exists];
+      newCart = [...remaining, exists];
     }
     setCart(newCart);
     addToDb(product._id)
@@ -70,29 +87,54 @@ const Shop = () => {
 
   }
 
+  const handleSelectChange = (event) => {
+    setItemsPerPage(event.target.value)
+  }
+
   return (
-    <div className='shop-container'>
-      <div className="products-container">
-        {
-          products.map(product => <Product
-            key={product._id}
-            product={product}
-            handleAddToCart={handleAddToCart}
-          ></Product>)
-        }
+    <>
+      <div className='shop-container'>
+        <div className="products-container">
+          {
+            products.map(product => <Product
+              key={product._id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>)
+          }
+
+        </div>
+        <div className="cart-container">
+          <Cart
+            cart={cart}
+            handleClearCart={handleClearCart}
+          >
+            <Link className='proceed-link' to="/orders">  <button className='btn-checkout'>Review Order <FontAwesomeIcon icon={faArrowRight} /> </button></Link>
+
+          </Cart>
+        </div>
 
       </div>
-      <div className="cart-container">
-        <Cart 
-        cart={cart}
-        handleClearCart={handleClearCart}
-        >
-          <Link className='proceed-link' to="/orders">  <button className='btn-checkout'>Review Order <FontAwesomeIcon icon={faArrowRight}/> </button></Link>
-          
-        </Cart>
-      </div>
+      {/* pagination */}
+      <div className="pagination">
+        <p>currentPage:{currentPage}</p>
+          {
+            pageNumber.map(number => <button 
+              key={number}
+              className={currentPage === number ? 'selected' : ''}
+              onClick={()=> setCurrentPage(number)}
+              >{number}</button>)
+          }
+          <select value={itemsPerPage} onChange={handleSelectChange}>
+            {
+              options.map(option => (
+              <option value={option}>{option}
+              </option>
+              ))}
 
-    </div>
+          </select>
+      </div>
+    </>
   )
 }
 
